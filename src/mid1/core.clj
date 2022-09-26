af7edf384de5adc236cb48ebf9a650e1cc06e1fa
gpsoft <etoh.gpsoft@gmail.com>
2022-09-27 08:42:36 +0900

    Change sound font

--- src/mid1/core.clj
+++ src/mid1/core.clj
@@ -4,17 +4,21 @@
     [uncomplicate.clojure-sound.core :as sound]
     [uncomplicate.clojure-sound.midi :as midi]
     [uncomplicate.commons.core :refer [close!]])
+  (:import
+    [com.sun.media.sound SF2Soundbank])
   (:gen-class))
 
 (def maple (midi/sequence (io/resource "maple.mid")))
 (def fluid (midi/soundbank (io/resource "FluidR3_GM.sf2")))
+(def salamander (midi/soundbank (io/resource "SalamanderGrandPiano.sf2")))
 
 (def sqcr (midi/sequencer))
 
 (comment
   (do (def synth (midi/synthesizer))
       (sound/open! synth)
-      (midi/load! synth fluid)
+      #_(midi/load! synth fluid)
+      (midi/load! synth salamander)
       (sound/connect! sqcr synth))
   )
 
