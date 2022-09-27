8f0e34cb39937d65c68eb64b67050df6ea8993d6
gpsoft <etoh.gpsoft@gmail.com>
2022-09-27 13:09:07 +0900

    Use primitive APIs

--- src/mid1/core.clj
+++ src/mid1/core.clj
@@ -1,32 +1,106 @@
 (ns mid1.core
   (:require
     [clojure.java.io :as io]
-    [uncomplicate.clojure-sound.core :as sound]
+    #_[uncomplicate.clojure-sound.core :as sound]
     [uncomplicate.clojure-sound.midi :as midi]
-    [uncomplicate.commons.core :refer [close!]])
+    #_[uncomplicate.commons.core :refer [close!]])
   (:import
-    [com.sun.media.sound SF2Soundbank])
+    [javax.sound.midi MidiSystem ShortMessage]
+    #_[javax.sound.sampled AudioSystem]
+    #_[com.sun.media.sound SF2Soundbank])
   (:gen-class))
 
-(def maple (midi/sequence (io/resource "maple.mid")))
-(def fluid (midi/soundbank (io/resource "FluidR3_GM.sf2")))
-(def salamander (midi/soundbank (io/resource "SalamanderGrandPiano.sf2")))
-
-(def sqcr (midi/sequencer))
 
 (comment
-  (do (def synth (midi/synthesizer))
-      (sound/open! synth)
-      #_(midi/load! synth fluid)
-      (midi/load! synth salamander)
-      (sound/connect! sqcr synth))
+  (def defaultSynthesizer (MidiSystem/getSynthesizer))
+  ; {:class "SoftSynthesizer"
+  ;  :status :closed
+  ;  :micro-position 0
+  ;  :description "Software MIDI Synthesizer"
+  ;  :name "Gervill"
+  ;  :vendor "OpenJDK"
+  ;  :version "1.0"}
+
+  (count (.getLoadedInstruments defaultSynthesizer))
+  ; 0
+  (.open defaultSynthesizer)
+  (count (.getLoadedInstruments defaultSynthesizer))
+  ; 129
+  (.getName (first (.getLoadedInstruments defaultSynthesizer)))
+  ; "Acoustic Grand Piano"
+  (.getName (nth (.getLoadedInstruments defaultSynthesizer) 6))
+  ; "Harpsichord"
+  (.getPatch (nth (.getLoadedInstruments defaultSynthesizer ) 6))
+  ; {:bank 0, :program 6}
+
+  (.getProgram (first (.getChannels defaultSynthesizer)))
+  ; 0
+  (.send (.getReceiver defaultSynthesizer) (ShortMessage. ShortMessage/NOTE_ON 0 60 50) -1)
+  ; ♪Grand piano sound
+
+  (.programChange (first (.getChannels defaultSynthesizer)) 100)
+  (.send (.getReceiver defaultSynthesizer) (ShortMessage. ShortMessage/NOTE_ON 0 60 50) -1)
+  #_(.send (.getReceiver defaultSynthesizer) (ShortMessage. ShortMessage/NOTE_OFF 0 60 50) -1)
+  ; ♪Harpsichord sound? maybe
+
+  ;; sample midi data
+  (def maple (MidiSystem/getSequence (io/resource "maple.mid")))
+
+  (def sequencer (MidiSystem/getSequencer))
+  ; {:class "RealTimeSequencer"
+  ; :status :closed
+  ; :micro-position 0
+  ; :description "Software sequencer"
+  ; :name "Real Time Sequencer"
+  ; :vendor "Oracle Corporation"
+  ; :version "Version 1.0"}
+
+  (.open sequencer)
+
+  (.getReceiver (.getTransmitter sequencer))
+  ; nil
+  ; レシーバがつながってない。
+
+  (.getSequence sequencer)
+  ; nil
+  (.setSequence sequencer maple)
+  (future
+    (.start sequencer)
+    (Thread/sleep 2000)
+    (.stop sequencer))
+  ; でも音は鳴る。なんで?
+
+  (.setReceiver (.getTransmitter sequencer) (.getReceiver defaultSynthesizer))
+  ; レシーバをつなげる
+  (future
+    (.start sequencer)
+    (Thread/sleep 2000)
+    (.stop sequencer))
+  ; ハープシコード…かな?
   )
 
-(sound/open! sqcr)
-(midi/sequence! sqcr maple)
 
-(sound/start! sqcr)
-(sound/stop! sqcr)
+
+;;(def maple (midi/sequence (io/resource "maple.mid")))
+;;(def fluid (midi/soundbank (io/resource "FluidR3_GM.sf2")))
+;;(def salamander (midi/soundbank (io/resource "SalamanderGrandPiano.sf2")))
+;;#_(def salamander (SF2Soundbank. (io/file "SalamanderGrandPiano/SalamanderGrandPiano.sfz")))
+;;
+;;(def sqcr (midi/sequencer))
+;;
+;;(comment
+;;  (do (def synth (midi/synthesizer))
+;;      (sound/open! synth)
+;;      #_(midi/load! synth fluid)
+;;      (midi/load! synth salamander)
+;;      (sound/connect! sqcr synth))
+;;  )
+;;
+;;(sound/open! sqcr)
+;;(midi/sequence! sqcr maple)
+;;
+;;(sound/start! sqcr)
+;;(sound/stop! sqcr)
 
 (defn -main
   "I don't do a whole lot ... yet."
