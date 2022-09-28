6bd0d951db4cd3775fb430ab7e3d992f22621ffd
gpsoft <etoh.gpsoft@gmail.com>
2022-09-28 10:22:58 +0900

    Refactoring

--- src/mid1/core.clj
+++ src/mid1/core.clj
@@ -1,212 +1,48 @@
 (ns mid1.core
   (:require
     [clojure.java.io :as io]
-    #_[uncomplicate.clojure-sound.core :as sound]
-    [uncomplicate.clojure-sound.midi :as midi]
-    #_[uncomplicate.commons.core :refer [close!]])
+    [mid1.midi :as midi])
   (:import
-    [javax.sound.midi MidiSystem ShortMessage Sequencer Synthesizer Sequence]
-    #_[javax.sound.sampled AudioSystem]
-    #_[com.sun.media.sound SF2Soundbank])
+    [javax.sound.midi MidiSystem ShortMessage Sequencer Synthesizer Sequence])
   (:gen-class))
 
+(defn open!
+  []
+  (let [pcspkr (midi/open-pcspkr!)
+        recorder (midi/open-recorder!)
+        d1-in (midi/open-d1-in!)]
+    [pcspkr recorder d1-in]))
+
+(defn close!
+  [devices]
+  (dorun (for [dev devices]
+           (when dev (midi/close-dev! dev)))))
 
 (comment
-  (def defaultSynthesizer (MidiSystem/getSynthesizer))
-  ; {:class "SoftSynthesizer"
-  ;  :status :closed
-  ;  :micro-position 0
-  ;  :description "Software MIDI Synthesizer"
-  ;  :name "Gervill"
-  ;  :vendor "OpenJDK"
-  ;  :version "1.0"}
-
-  (count (.getLoadedInstruments defaultSynthesizer))
-  ; 0
-  (.open defaultSynthesizer)
-  (count (.getLoadedInstruments defaultSynthesizer))
-  ; 129
-  (.getDefaultSoundbank defaultSynthesizer)
-  ; {:class "SF2Soundbank"
-  ;  :description "Emergency generated soundbank"
-  ;  :name "Emergency GM sound set"
-  ;  :vendor "Generated"
-  ;  :version "2.1"}
-  (.getName (first (.getLoadedInstruments defaultSynthesizer)))
-  ; "Acoustic Grand Piano"
-  (.getName (nth (.getLoadedInstruments defaultSynthesizer) 6))
-  ; "Harpsichord"
-  (.getPatch (nth (.getLoadedInstruments defaultSynthesizer ) 6))
-  ; {:bank 0, :program 6}
-
-  (.getProgram (first (.getChannels defaultSynthesizer)))
-  ; 0
-  (.send (.getReceiver defaultSynthesizer) (ShortMessage. ShortMessage/NOTE_ON 0 60 50) -1)
-  ; ♪Grand piano sound
-
-  (.programChange (first (.getChannels defaultSynthesizer)) 6)
-  (.send (.getReceiver defaultSynthesizer) (ShortMessage. ShortMessage/NOTE_ON 0 60 50) -1)
-  ; ♪Harpsichord sound? maybe
-
-  ;; sample midi data
-  (def maple (MidiSystem/getSequence (io/resource "maple.mid")))
-
-  (def sequencer (MidiSystem/getSequencer))
-  ; {:class "RealTimeSequencer"
-  ;  :status :closed
-  ;  :micro-position 0
-  ;  :description "Software sequencer"
-  ;  :name "Real Time Sequencer"
-  ;  :vendor "Oracle Corporation"
-  ;  :version "Version 1.0"}
-
-  (count (.getTransmitters sequencer))
-  ; 1
-
-  (def tx (first (.getTransmitters sequencer)))
-  (.getReceiver tx)
-  ; {:class "SoftReceiver", :id 2064131581}
-  ; どこにつながってるのか良くわからん。
-
-  ;; (.getTransmitter sequencer)すると、そのたびに新しいトランスミッタが生成される。
-  ;; 既存のトランスミッタを使うなら、getTransmittersしてnthする方がいいのかな?
-
-  (.open sequencer)
-
-  (.getSequence sequencer)
-  ; nil
-  (.setSequence sequencer maple)
-  (future
-    (.start sequencer)
-    (Thread/sleep 2000)
-    (.stop sequencer))
-  ; グランドピアノの音。
-  ; どのシンセサイザが音を鳴らしてるのか?
-
-  (.setReceiver tx (.getReceiver defaultSynthesizer))
-  ; シーケンサのトランスミッタをdefaultSynthesizerへつなげる
-  ; sequencer→synthesizer
-  (future
-    (.setTickPosition sequencer 0)
-    (.start sequencer)
-    (Thread/sleep 2000)
-    (.stop sequencer))
-  ; ハープシコード…かなぁ?
-
-  (.unloadAllInstruments defaultSynthesizer (.getDefaultSoundbank defaultSynthesizer ))
-  (def fluid (MidiSystem/getSoundbank (io/resource "FluidR3_GM.sf2")))
-  (.loadAllInstruments defaultSynthesizer fluid)
-  (dorun
-    (map (fn [inst]
-           (let [prg (.getProgram (.getPatch inst))]
-             (future
-               (Thread/sleep (* 2200 prg))
-               (prn (.getName inst))
-               (.programChange (first (.getChannels defaultSynthesizer)) prg)
-               #_(.setTickPosition sequencer 0)
-               (.start sequencer)
-               (Thread/sleep 2000)
-               (.stop sequencer))))
-         (take 40 (.getLoadedInstruments defaultSynthesizer))))
-
-  (def salamander (MidiSystem/getSoundbank (io/resource "SalamanderGrandPiano.sf2")))
-  (.unloadAllInstruments defaultSynthesizer fluid)
-  (.loadInstrument defaultSynthesizer (first (.getInstruments salamander)))
-  (future
-    (.setTickPosition sequencer 0)
-    (.start sequencer)
-    (Thread/sleep 10000)
-    (.stop sequencer))
+  (let [devs (open!)
+        [pcspkr recorder d1-in] devs
+        [empty-media track] (midi/new-media! :scratch nil)
+        path "/var/tmp/sample.mid"]
+    (when d1-in
+      (midi/connect! d1-in pcspkr)
+      (midi/connect! d1-in recorder)
+      (midi/prepare-rec! recorder empty-media)
+      (midi/start-rec!
+        recorder 60000
+        #(do
+           (midi/save! recorder path)
+           (close! devs)))))
+  (let [devs (open!)
+        [pcspkr recorder] devs
+        path "/var/tmp/sample.mid"]
+    (let [m (midi/new-media! :file path)]
+      (midi/connect! recorder pcspkr)
+      (midi/play!
+        recorder m 60000
+        #(close! devs))))
   )
 
-(defn dev-type
-  [dev]
-  (let [sqcr (instance? Sequencer dev)
-        synth (instance? Synthesizer dev)
-        port (not (or sqcr synth))
-        in (zero? (.getMaxReceivers dev))]
-    (cond
-      sqcr :sequencer
-      synth :synthesizer
-      in :in-port
-      :else :out-port)))
 
-(defn render-dev
-  [dev]
-  (let [klass (.getClass dev)
-        id (System/identityHashCode dev)
-        info (.getDeviceInfo dev)
-        hash (.hashCode info)
-        name (.getName info)
-        open (.isOpen dev)
-        type (dev-type dev)
-        rxs (count (.getReceivers dev))
-        txs (count (.getTransmitters dev))]
-    (format "%s(%s) TX:%d,RX:%d %s" name (if open "open" "close") txs rxs type)))
-
-(defn devices
-  []
-  (let [infos (MidiSystem/getMidiDeviceInfo)
-        devs (map #(MidiSystem/getMidiDevice %) infos)]
-    (dorun
-      (map-indexed #(println (format "[%d]%s" %1 (render-dev %2))) devs))))
-
-(defn piano
-  []
-  (let [dev (MidiSystem/getSynthesizer)
-        sb (MidiSystem/getSoundbank (io/resource "SalamanderGrandPiano.sf2"))]
-    (.open dev)
-    (.loadAllInstruments dev sb)
-    dev))
-
-(defn in-port
-  []
-  (let [infos (MidiSystem/getMidiDeviceInfo)
-        devs (map #(MidiSystem/getMidiDevice %) infos)
-        dev (first (filter #(= (dev-type %) :in-port) devs))]
-    (when dev
-      (.open dev))
-    dev))
-
-(defn connect
-  [from to]
-  (let [tx (.getTransmitter from)]
-    (.setReceiver tx (.getReceiver to))))
-
-(comment
-  (let [synth (piano)
-        in (in-port)
-        sqcr (MidiSystem/getSequencer)
-        _ (.open sqcr)
-        _ (connect sqcr synth)
-        empty-seq (Sequence. Sequence/PPQ 10)
-        - (println (MidiSystem/getMidiFileTypes empty-seq))
-        track (.createTrack empty-seq)
-        _ (.setSequence sqcr empty-seq)
-        _ (.recordEnable sqcr track 0)]
-    (if in
-      (do (connect in sqcr)
-          (println "size:" (.size track))
-          (.startRecording sqcr)
-          (future
-            (Thread/sleep 10000)
-            (.stopRecording sqcr)
-            (println "size:" (.size track))
-            (MidiSystem/write empty-seq 1 (io/file "/var/tmp/sample.mid"))
-            (.close in)
-            (.close sqcr)
-            (.close synth)))
-      (do
-        (.setSequence sqcr (MidiSystem/getSequence (io/file "/var/tmp/sample.mid")))
-        (future
-          (.setTickPosition sqcr 0)
-          (.start sqcr)
-          (Thread/sleep 10000)
-          (.stop sqcr)
-          (.close sqcr)
-          (.close synth))))
-    )
-  )
 
 (defn -main
   "I don't do a whole lot ... yet."
---
+++ src/mid1/midi.clj
@@ -0,0 +1,228 @@
+(ns mid1.midi
+  (:require
+    [clojure.java.io :as io]
+    #_[uncomplicate.clojure-sound.core :as sound]
+    [uncomplicate.clojure-sound.midi :as midi]
+    #_[uncomplicate.commons.core :refer [close!]])
+  (:import
+    [javax.sound.midi MidiSystem ShortMessage Sequencer Synthesizer Sequence]
+    #_[javax.sound.sampled AudioSystem]
+    #_[com.sun.media.sound SF2Soundbank]))
+
+(defn dev-type
+  [dev]
+  (let [sqcr (instance? Sequencer dev)
+        synth (instance? Synthesizer dev)
+        port (not (or sqcr synth))
+        in (zero? (.getMaxReceivers dev))]
+    (cond
+      sqcr :sequencer
+      synth :synthesizer
+      in :in-port
+      :else :out-port)))
+
+(defn render-dev
+  [dev]
+  (let [klass (.getClass dev)
+        id (System/identityHashCode dev)
+        info (.getDeviceInfo dev)
+        hash (.hashCode info)
+        name (.getName info)
+        open (.isOpen dev)
+        type (dev-type dev)
+        rxs (count (.getReceivers dev))
+        txs (count (.getTransmitters dev))]
+    (format "%s(%s) TX:%d,RX:%d %s" name (if open "open" "close") txs rxs type)))
+
+(defn devices
+  []
+  (let [infos (MidiSystem/getMidiDeviceInfo)
+        devs (map #(MidiSystem/getMidiDevice %) infos)]
+    (dorun
+      (map-indexed #(println (format "[%d]%s" %1 (render-dev %2))) devs))))
+
+(defn close-dev!
+  [dev]
+  (.close dev))
+
+(defn open-pcspkr!
+  []
+  (let [dev (MidiSystem/getSynthesizer)
+        sb (MidiSystem/getSoundbank (io/resource "SalamanderGrandPiano.sf2"))]
+    (.open dev)
+    (.loadAllInstruments dev sb)
+    dev))
+
+(defn open-recorder!
+  []
+  (let [dev (MidiSystem/getSequencer)]
+    (.open dev)
+    dev))
+
+(defn open-d1-in!
+  []
+  (let [infos (MidiSystem/getMidiDeviceInfo)
+        devs (map #(MidiSystem/getMidiDevice %) infos)
+        dev (first (filter #(= (dev-type %) :in-port) devs))]
+    (when dev
+      (.open dev))
+    dev))
+
+(defn connect!
+  [from to]
+  (let [tx (.getTransmitter from)]
+    (.setReceiver tx (.getReceiver to))))
+
+(defn new-media!
+  [from path]
+  (case from
+    :resource (MidiSystem/getSequence (io/resource path))
+    :file (MidiSystem/getSequence (io/file path))
+    :scratch (let [s (Sequence. Sequence/PPQ 10)
+                   t (.createTrack s)]
+               [s t])
+    nil))
+
+(defn play!
+  [recorder s elapse cc]
+  (.setSequence recorder s)
+  (.setTickPosition recorder 0)
+  (future
+    (.start recorder)
+    (Thread/sleep elapse)
+    (.stop recorder)
+    (cc)))
+
+(defn prepare-rec!
+  [recorder s]
+  (.setSequence recorder s)
+  (let [t (first (.getTracks s))]
+    (.recordEnable recorder t 0))
+  (.setTickPosition recorder 0)
+  nil)
+
+(defn start-rec!
+  [recorder elapse cc]
+  (future
+    (.startRecording recorder)
+    (Thread/sleep elapse)
+    (.stopRecording recorder)
+    (cc)))
+
+(defn save!
+  [recorder path]
+  (let [s (.getSequence recorder)
+        t (first (MidiSystem/getMidiFileTypes s))]
+    (MidiSystem/write s t (io/file path))))
+
+
+(comment
+  (def defaultSynthesizer (MidiSystem/getSynthesizer))
+  ; {:class "SoftSynthesizer"
+  ;  :status :closed
+  ;  :micro-position 0
+  ;  :description "Software MIDI Synthesizer"
+  ;  :name "Gervill"
+  ;  :vendor "OpenJDK"
+  ;  :version "1.0"}
+
+  (count (.getLoadedInstruments defaultSynthesizer))
+  ; 0
+  (.open defaultSynthesizer)
+  (count (.getLoadedInstruments defaultSynthesizer))
+  ; 129
+  (.getDefaultSoundbank defaultSynthesizer)
+  ; {:class "SF2Soundbank"
+  ;  :description "Emergency generated soundbank"
+  ;  :name "Emergency GM sound set"
+  ;  :vendor "Generated"
+  ;  :version "2.1"}
+  (.getName (first (.getLoadedInstruments defaultSynthesizer)))
+  ; "Acoustic Grand Piano"
+  (.getName (nth (.getLoadedInstruments defaultSynthesizer) 6))
+  ; "Harpsichord"
+  (.getPatch (nth (.getLoadedInstruments defaultSynthesizer ) 6))
+  ; {:bank 0, :program 6}
+
+  (.getProgram (first (.getChannels defaultSynthesizer)))
+  ; 0
+  (.send (.getReceiver defaultSynthesizer) (ShortMessage. ShortMessage/NOTE_ON 0 60 50) -1)
+  ; ♪Grand piano sound
+
+  (.programChange (first (.getChannels defaultSynthesizer)) 6)
+  (.send (.getReceiver defaultSynthesizer) (ShortMessage. ShortMessage/NOTE_ON 0 60 50) -1)
+  ; ♪Harpsichord sound? maybe
+
+  ;; sample midi data
+  (def maple (MidiSystem/getSequence (io/resource "maple.mid")))
+
+  (def sequencer (MidiSystem/getSequencer))
+  ; {:class "RealTimeSequencer"
+  ;  :status :closed
+  ;  :micro-position 0
+  ;  :description "Software sequencer"
+  ;  :name "Real Time Sequencer"
+  ;  :vendor "Oracle Corporation"
+  ;  :version "Version 1.0"}
+
+  (count (.getTransmitters sequencer))
+  ; 1
+
+  (def tx (first (.getTransmitters sequencer)))
+  (.getReceiver tx)
+  ; {:class "SoftReceiver", :id 2064131581}
+  ; どこにつながってるのか良くわからん。
+
+  ;; (.getTransmitter sequencer)すると、そのたびに新しいトランスミッタが生成される。
+  ;; 既存のトランスミッタを使うなら、getTransmittersしてnthする方がいいのかな?
+
+  (.open sequencer)
+
+  (.getSequence sequencer)
+  ; nil
+  (.setSequence sequencer maple)
+  (future
+    (.start sequencer)
+    (Thread/sleep 2000)
+    (.stop sequencer))
+  ; グランドピアノの音。
+  ; どのシンセサイザが音を鳴らしてるのか?
+
+  (.setReceiver tx (.getReceiver defaultSynthesizer))
+  ; シーケンサのトランスミッタをdefaultSynthesizerへつなげる
+  ; sequencer→synthesizer
+  (future
+    (.setTickPosition sequencer 0)
+    (.start sequencer)
+    (Thread/sleep 2000)
+    (.stop sequencer))
+  ; ハープシコード…かなぁ?
+
+  (.unloadAllInstruments defaultSynthesizer (.getDefaultSoundbank defaultSynthesizer ))
+  (def fluid (MidiSystem/getSoundbank (io/resource "FluidR3_GM.sf2")))
+  (.loadAllInstruments defaultSynthesizer fluid)
+  (dorun
+    (map (fn [inst]
+           (let [prg (.getProgram (.getPatch inst))]
+             (future
+               (Thread/sleep (* 2200 prg))
+               (prn (.getName inst))
+               (.programChange (first (.getChannels defaultSynthesizer)) prg)
+               #_(.setTickPosition sequencer 0)
+               (.start sequencer)
+               (Thread/sleep 2000)
+               (.stop sequencer))))
+         (take 40 (.getLoadedInstruments defaultSynthesizer))))
+
+  (def salamander (MidiSystem/getSoundbank (io/resource "SalamanderGrandPiano.sf2")))
+  (.unloadAllInstruments defaultSynthesizer fluid)
+  (.loadInstrument defaultSynthesizer (first (.getInstruments salamander)))
+  (future
+    (.setTickPosition sequencer 0)
+    (.start sequencer)
+    (Thread/sleep 10000)
+    (.stop sequencer))
+  )
+
+(comment
+  )
